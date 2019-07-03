00905e246b3d8c2c11c66436dc219ed1a9cf7360
SongJemin <jemin56@naver.com>
2019-07-04 01:46:57 +0900

    Add ui_artist_song_fragment

--- app/src/main/java/com/song2/wave/UI/Main/Search/SearchArtistFragment.kt
+++ app/src/main/java/com/song2/wave/UI/Main/Search/SearchArtistFragment.kt
@@ -2,19 +2,30 @@ package com.song2.wave.UI.Main.Search
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.song2.wave.Data.model.Search.SongData
 import com.song2.wave.R
 import com.song2.wave.UI.Main.MainActivity
+import com.song2.wave.UI.Main.Search.Adapter.SongSearchAdapter
 import com.song2.wave.Util.Interface.OnBackPressedListener
+import kotlinx.android.synthetic.main.fragment_artist_song.view.*
+import kotlinx.android.synthetic.main.fragment_search_home.view.*
 
 
 class SearchArtistFragment : android.support.v4.app.Fragment(), OnBackPressedListener{
 
     var TAG = "SearchArtistFragment"
+    lateinit var songDataArr : ArrayList<SongData>
     var searchHomeFragment = SearchHomeFragment()
+    lateinit var songSearchAdapter : SongSearchAdapter
+    lateinit var songFieldData : ArrayList<String>
+    lateinit var requestManager : RequestManager
 
     override fun onBackPressed() {
         Log.e(TAG, "onBack()")
@@ -27,6 +38,14 @@ class SearchArtistFragment : android.support.v4.app.Fragment(), OnBackPressedLis
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_artist_song,container,false)
 
+        requestManager = Glide.with(this)
+        songDataArr = ArrayList<SongData>()
+        insertExampleData()
+
+        songSearchAdapter = SongSearchAdapter(songDataArr, requestManager)
+        v.recycler_search_artist_frag_song.adapter = songSearchAdapter
+        v.recycler_search_artist_frag_song.layoutManager = LinearLayoutManager(v.context)
+
         return v
     }
 
@@ -35,6 +54,17 @@ class SearchArtistFragment : android.support.v4.app.Fragment(), OnBackPressedLis
         super.onAttach(context)
         Log.e(TAG, "onAttach()")
         (context as MainActivity).setOnBackPressedListener(this, 1)
+    }
+
+    fun insertExampleData(){
+        songFieldData = ArrayList<String>()
+        songFieldData.add("분야1")
+
+        songDataArr.add(SongData("https://t1.daumcdn.net/cfile/tistory/2442394558BBBD1934", "좋은날", "아이유(IU)", "송제민", songFieldData))
+        songDataArr.add(SongData("https://t1.daumcdn.net/cfile/tistory/2442394558BBBD1934", "좋은날", "아이유(IU)", "송제민", songFieldData))
+        songDataArr.add(SongData("http://cdn.news2day.co.kr/news-images/peg/news/201709/8r1YZtmQRWoSic7Q6fv6i3cnEuj2RP0sqJwwEWGa-wm-1505700400.jpg", "가을아침", "아이유(IU)", "송제민", songFieldData))
+        songDataArr.add(SongData("http://cdn.news2day.co.kr/news-images/peg/news/201709/8r1YZtmQRWoSic7Q6fv6i3cnEuj2RP0sqJwwEWGa-wm-1505700400.jpg", "가을아침", "아이유(IU)", "송제민", songFieldData))
+
     }
 
 }
