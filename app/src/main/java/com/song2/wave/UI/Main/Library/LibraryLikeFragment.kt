148f7b61246129f53f0b9b7e7abf4e23cd65a4e4
SongJemin <jemin56@naver.com>
2019-07-04 19:39:16 +0900

    Add ui Library Fragment

---
+++ app/src/main/java/com/song2/wave/UI/Main/Library/LibraryLikeFragment.kt
@@ -0,0 +1,17 @@
+package com.song2.wave.UI.Main.Library
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LibraryLikeFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var v: View = inflater.inflate(com.song2.wave.R.layout.fragment_library_like, container, false)
+        return v
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
