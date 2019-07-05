73f851fabfee6ee57a4ff0e60b449ae4514b600f
leesungeun <selee214@gmail.com>
2019-07-05 23:01:57 +0900

    Modify git merge error

--- app/src/main/java/com/song2/wave/UI/Main/Home/Adapter/WaitingSongHomeViewHolder.kt
+++ app/src/main/java/com/song2/wave/UI/Main/Home/Adapter/WaitingSongHomeViewHolder.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import com.song2.wave.R
 
 class WaitingSongHomeViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-
     var waitingSongContainer : RelativeLayout = itemView!!.findViewById(R.id.rl_item_my_waiting_song_container)
     var songCoverImg: ImageView = itemView!!.findViewById(R.id.iv_item_waiting_song_cover_img)
     var songInfo : TextView = itemView!!.findViewById(R.id.tv_item_waiting_song_item_song_info)
