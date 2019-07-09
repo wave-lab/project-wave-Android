db7e41cca63042ebf60e8825913e4d3dfcfbed46
seunghee63 <0603yang@naver.com>
2019-07-10 07:43:20 +0900

    Add mp3 file upload & song info

--- app/src/main/java/com/song2/wave/UI/Main/MyPage/SetStartPointActivity.kt
+++ app/src/main/java/com/song2/wave/UI/Main/MyPage/SetStartPointActivity.kt
@@ -1,27 +1,27 @@
 package com.song2.wave.UI.Main.MyPage
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.SeekBar
-import android.widget.Toast
 import com.song2.wave.R
 import kotlinx.android.synthetic.main.activity_set_start_point.*
+import org.jetbrains.anko.startActivity
 import java.util.*
 
 class SetStartPointActivity : AppCompatActivity() {
-/*
+
     lateinit var playTime: String
     lateinit var seekbar: SeekBar
 
     var playbackPosition = 0
     var currentPosition = 0
 
-
-    var isPlaying = false*/
+    var isStart = false
+    var isComplete = false
+    var isPlaying = false
 
     var mediaPlayer = MediaPlayer()
 
@@ -30,41 +30,70 @@ class SetStartPointActivity : AppCompatActivity() {
         setContentView(R.layout.activity_set_start_point)
 
         var upload_song_uri: String = intent.getStringExtra("musicDataURI")
-        mediaPlayer.setDataSource(getApplicationContext(), Uri.parse(upload_song_uri))
-        //mediaPlayer.setDataSource(this, Uri.parse(upload_song_uri))
 
-        mediaPlayer.prepare()
-        mediaPlayer.start()
+        initialSetting(upload_song_uri)
 
+        iv_set_start_act_play_btn.setOnClickListener {
+            Log.e("seekbarSelectorBefore", sb_set_start_act_seekbar.isSelected.toString())
 
-        //addSeekBar()
-        //setSeekbarUI()
+            if (iv_set_start_act_play_btn.isSelected == false && isPlaying == false)
+                playAudio()
+            else if (!iv_set_start_act_play_btn.isSelected)
+                restart()
+            else
+                pauseAudio()
 
+            iv_set_start_act_play_btn.isSelected = !iv_set_start_act_play_btn.isSelected
+            Log.e("seekbarSelector", sb_set_start_act_seekbar.isSelected.toString())
+        }
+
+        iv_set_start_act_upload_btn.setOnClickListener {
+            try {
+                startActivity<UpLoadSongCoverActivity>("StartPoint" to currentPosition.toString(), "songURI" to upload_song_uri)
+                finish()
+                //killMediaPlayer()
+                Log.e("startPoint", currentPosition.toString())
+            } catch (e: Exception) {
+
+            }
+        }
     }
 
-/*    fun setSeekbarUI(){
-        fun addTimer(){
-            val tt = object : TimerTask() {
-                override fun run() {
-                    runOnUiThread {
-                        playTime = String.format(
-                            "%02d:%02d", ((mediaPlayer.getCurrentPosition() / 1000) % 3600 / 60),
-                            ((mediaPlayer.getCurrentPosition() / 1000) % 3600 % 60))
+    fun addTimer() {
+        val tt = object : TimerTask() {
 
-                        tv_set_start_act_duration_time.setText(playTime)
-                        seekbar.setProgress(mediaPlayer.getCurrentPosition())
-                    }
+            override fun run() {
+                currentPosition = mediaPlayer.getCurrentPosition()
+                runOnUiThread {
+                    playTime = String.format(
+                        "%02d:%02d", ((currentPosition / 1000) % 3600 / 60),
+                        ((currentPosition / 1000) % 3600 % 60)
+                    )
+                    tv_set_start_act_duration_time.setText(playTime)
+                    seekbar.setProgress(currentPosition)
                 }
             }
-
-            /////////// / Timer 생성 //////////////
-            val timer = Timer()
-            timer.schedule(tt, 0, 1000)
-            //////////////////////////////////////
-
         }
-    }*/
-/*
+        val timer = Timer()
+        timer.schedule(tt, 0, 1000)
+    }
+
+    fun initialSetting(upload_song_uri : String){
+
+        mediaPlayer.setDataSource(getApplicationContext(), Uri.parse(upload_song_uri))
+        mediaPlayer.prepare()
+
+        addSeekBar()
+        addTimer()
+
+        var play_duration = mediaPlayer.getDuration()
+        var lengthOfSong =
+            String.format("%02d:%02d", ((play_duration / 1000) % 3600 / 60), ((play_duration / 1000) % 3600 % 60))
+        tv_set_start_act_length_of_song.setText(lengthOfSong)
+
+        seekbar.setMax(play_duration)
+    }
+
     //seekbar touchListener
     fun addSeekBar() {
         seekbar = sb_set_start_act_seekbar
@@ -73,17 +102,20 @@ class SetStartPointActivity : AppCompatActivity() {
             //값을 변경 한 후 터치를 떼었을 때
             override fun onStopTrackingTouch(seekbar: SeekBar) {
                 isPlaying = true
-
                 playbackPosition = seekbar.progress
                 mediaPlayer.seekTo(playbackPosition)
 
-                if(sb_set_start_act_seekbar.isSelected){
+                Log.e("onStopTrackingTouch", sb_set_start_act_seekbar.isSelected.toString())
+                if (!sb_set_start_act_seekbar.isSelected) {
                     mediaPlayer.start()
-                }
+                } else
+                    mediaPlayer.pause()
             }
 
             //seek바의 값을 변경하기 위해 터치했을 때
             override fun onStartTrackingTouch(seekbar: SeekBar) {
+                Log.e("onStartTrackingTouch", sb_set_start_act_seekbar.isSelected.toString())
+
                 isPlaying = false
                 mediaPlayer.pause()
             }
@@ -96,43 +128,44 @@ class SetStartPointActivity : AppCompatActivity() {
                 }
             }
         })
-    }*/
-/*
+    }
 
-    //미디어를 재생하는 사용자 정의 메소드
-    fun playAudio(url: String) {
+    //미디어를 재생 메소드
+    fun playAudio() {
 
-        var play_duration = mediaPlayer.getDuration()
-        var lengthOfSong =
-            String.format("%02d:%02d", ((play_duration/ 1000) % 3600 / 60), ((play_duration / 1000) % 3600 % 60))
+        try {
+            mediaPlayer.start()
+            isPlaying = true
+        } catch (e: Exception) {
+            Log.e("first_start", e.toString())
+            finish()
+        }
 
-        tv_set_start_act_length_of_song.setText(lengthOfSong)
+    }
 
-        isPlaying = true
-
-        seekbar.setMax(play_duration)
-        //seekBarThread.start()
+    fun pauseAudio() {
+        try {
+            mediaPlayer.pause()
+            isPlaying = false
+        } catch (e: Exception) {
+            Log.e("pause", e.toString())
+            finish()
+        }
     }
 
     fun restart() {
-
-        isPlaying = true // 재생하도록 flag 변경
-
-        mediaPlayer.seekTo(playbackPosition) // 일시정지 시점으로 이동
-        mediaPlayer.start() // 시작
-
-        //seekBarThread.run()
+        try {
+            mediaPlayer.start()
+            isPlaying = true
+        } catch (e: Exception) {
+            Log.e("start", e.toString())
+            finish()
+        }
     }
 
     fun killMediaPlayer() {
-*/
-/*      if (mediaPlayer != null && !mediaPlayer!!.isPlaying()) {
-            mediaPlayer!!.release()
-        }*//*
-
-        mediaPlayer!!.release()
+        mediaPlayer.release()
     }
-*/
 
 
 }
