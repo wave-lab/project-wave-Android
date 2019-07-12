8acc8be76f389366a3797f090e056c0d3400ebc6
seunghee63 <0603yang@naver.com>
2019-07-12 16:52:00 +0900

    Modify Just for test

--- app/src/main/java/com/song2/wave/Util/Network/NetworkService.kt
+++ app/src/main/java/com/song2/wave/Util/Network/NetworkService.kt
@@ -100,35 +100,41 @@ interface NetworkService {
     @GET("core/search")
     fun getSearchResponse(
         @Header("Content-Type") content_type: String,
-        @Query("originArtistName") originartistname : String?,
-        @Query("artistName") artistname : String?,
-        @Query("originTitle") origintitle : String?
+        @Query("originArtistName") originartistname: String?,
+        @Query("artistName") artistname: String?,
+        @Query("originTitle") origintitle: String?
 
-    ) : Call<GetSearchResponse>
+    ): Call<GetSearchResponse>
 
 
     @GET("core/users/emailCheck")
     fun getEmailCheckResponse(
-            @Query("email") email : String?
-    ) : Call<GetEmailCheckResponse>
+        @Query("email") email: String?
+    ): Call<GetEmailCheckResponse>
 
     @GET("core/users/nicknameCheck")
     fun getNicknameCheckResponse(
-            @Query("nickname") nickname : String?
-    ) : Call<GetNicknameCheckResponse>
+        @Query("nickname") nickname: String?
+    ): Call<GetNicknameCheckResponse>
 
     @GET("/pl/{playlistIdx}")
     fun getTop10PlaylistResponse(
         @Header("Content-Type") content_type: String,
         @Header("Authorization") authorization: String,
-        @Path("playlistIdx") playlistIdx : String?
-    ) : Call<GetTop10PlaylistResponse>
+        @Path("playlistIdx") playlistIdx: String?
+    ): Call<GetTop10PlaylistResponse>
 
     //** ds 작업 **//
     @GET("core/originArtist")
     fun getOriginArtistResponse(
         @Header("Content-Type") content_type: String
-    ) : Call<GetOriginArtistResponse>
+    ): Call<GetOriginArtistResponse>
+
+    @GET("/core/users/{userIdx}")
+    fun getArtistInfoRespnose(
+        @Header("Content-Type") content_type: String
+        @Path("userIdx") userIdx
+    ) : Call<GetArtistInfoRespnose>
 
     ////////////////////* POST *///////////////////////////
 
@@ -136,23 +142,23 @@ interface NetworkService {
     @Multipart
     @POST("/api/signup")
     fun postSignup(
-            @Part("email") email : RequestBody,
-            @Part("password") password : RequestBody,
-            @Part("nickname") nickname : RequestBody,
-            @Part profileImg : MultipartBody.Part?,
-            @Part("genre[0]") genre0 : RequestBody,
-            @Part("genre[1]") genre1 : RequestBody,
-            @Part("genre[2]") genre2 : RequestBody,
-            @Part("mood[0]") mood0 : RequestBody,
-            @Part("mood[1]") mood1 : RequestBody,
-            @Part("originArtist[0]") originArtist : RequestBody
-    ) : Call<PostResponse>
+        @Part("email") email: RequestBody,
+        @Part("password") password: RequestBody,
+        @Part("nickname") nickname: RequestBody,
+        @Part profileImg: MultipartBody.Part?,
+        @Part("genre[0]") genre0: RequestBody,
+        @Part("genre[1]") genre1: RequestBody,
+        @Part("genre[2]") genre2: RequestBody,
+        @Part("mood[0]") mood0: RequestBody,
+        @Part("mood[1]") mood1: RequestBody,
+        @Part("originArtist[0]") originArtist: RequestBody
+    ): Call<PostResponse>
 
     // 이메일 중복 확인
     @POST("/core/users/emailCheck")
     fun postEmailCheckResponse(
         @Header("Content-Type") content_type: String,
         @Body() body: JsonObject
-    ) : Call<PostResponse>
+    ): Call<PostResponse>
 
 }
